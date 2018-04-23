3f08a6f87261b7a0836c015425420550f384078c
Javier Calvarro Nelson <jacalvar@microsoft.com>
2018-04-23 13:21:55 -0700

    Add project files.

---
+++ BlazorApp/BlazorApp.Server/Hubs/GreetingHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorApp.Server.Hubs
+{
+    public class GreetingHub : Hub
+    {
+        public Task Greet(string name)
+        {
+            return Clients.Caller.SendAsync("Greeting", name);
+        }
+    }
+}
